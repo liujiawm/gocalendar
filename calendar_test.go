24998011a4a71cc2c3a9aa293e90925ea7c7fab3
phpu <liujiawm@163.com>
2020-03-14 04:12:38 +0800

    日历方式显示

--- calendar_test.go
+++ calendar_test.go
@@ -1,66 +1,23 @@
-// 年数在1000-3000
-
 package gocalendar
 
 import (
 	"testing"
 )
 
-var c = NewCtime()
-var d = CtimeNewDate(c)
+func TestGetMonthCalendar(t *testing.T) {
+	c := GetMonthCalendar(2020,1)
 
-// 24节气(从立春开始)
-func TestDate_Jieqi(t *testing.T) {
-	t.Log(NewCtime().Year(),"年从立春开始的节气时间：")
-	jq := d.Jieqi()
-	for k,v := range(jq){
-		t.Logf("%d:[%s] %d-%d-%d %d:%d:%d\n",k,v.Name,v.Date.Year,v.Date.Month,v.Date.Day,v.Date.Hour,v.Date.Min,v.Date.Sec)
+	t.Log(c.Year,"年",c.Month,"月")
+	t.Log("今天：",c.ToDay)
+	for _,v := range c.Items {
+		leapMonthStr := ""
+		if v.LunarDate.LeapMonth == 1 {
+			leapMonthStr = "(闰)"
+		}
+		t.Logf("公历年:%d年%d月%d日周%d 农历年:%d(%s%s)[%s]年%s%s月%s日 节气:%s",v.Date.Year,v.Date.Month,v.Date.Day,v.Date.Week,
+			v.LunarDate.Year, v.LunarDate.YearGanZi.Gan, v.LunarDate.YearGanZi.Zhi, v.LunarDate.YearGanZi.Animals,
+			leapMonthStr,MonthChinese(v.LunarDate.Month),DayChinese(v.LunarDate.Day),v.Date.JQ.Name)
 	}
-}
 
-// 公历的天干地支
-func TestDate_GanZhi(t *testing.T) {
-	/*d = NewDate(&Date{
-		Year:2020,
-		Month:2,
-		Day:4,
-		Hour:20,
-		Min:35,
-		Sec:42,
-		Loc:NewCtime().Location(),
-	})*/
-	Zwz = true
-	d.GanZhi()
-	t.Logf("公历 %d年%d月%d日%d时 转成以立春开始的干支是: %s%s年 %s%s月 %s%s日 %s%s时\n",d.Year,d.Month,d.Day,d.Hour,
-		TianGanArray[d.TianGanDiZhi.Ytg], DiZhiArray[d.TianGanDiZhi.Ydz],
-		TianGanArray[d.TianGanDiZhi.Mtg],DiZhiArray[d.TianGanDiZhi.Mdz],
-		TianGanArray[d.TianGanDiZhi.Dtg],DiZhiArray[d.TianGanDiZhi.Ddz],
-		TianGanArray[d.TianGanDiZhi.Htg],DiZhiArray[d.TianGanDiZhi.Hdz])
-	// t.Logf("%#v\n",dc.TianGanDiZhi)
-}
-
-// 公历转农历，农历转公历
-func TestDate_Solar2Lunar(t *testing.T) {
-	d := NewDate(&Date{
-		Year:2020,
-		Month:1,
-		Day:25,
-		Hour:5,
-		Min:35,
-		Sec:42,
-		Loc:NewCtime().Location(),
-	})
-	l := d.Solar2Lunar()
-	leapMonthStr := ""
-	if l.LeapMonth == 1 {
-		leapMonthStr = "(闰)"
-	}
-	t.Logf("公历%d年%d月%d日转成农历是:%d(%s%s)[%s]年%s%s月%s日",d.Year,d.Month,d.Day,l.Year,l.YearGanZi.Gan,l.YearGanZi.Zhi,l.YearGanZi.Animals, leapMonthStr,MonthChinese(l.Month),DayChinese(l.Day))
-
-	sd := l.Lunar2Solar()
-	t.Logf("农历:%d年%s%s月%s日转成公历是:%d年%d月%d日",l.Year,leapMonthStr,MonthChinese(l.Month),DayChinese(l.Day),sd.Year,sd.Month,sd.Day)
-}
-// 星座
-func TestDate_XingZuo(t *testing.T) {
-	t.Logf("公历%d年%d月%d日是%s座",d.Year,d.Month,d.Day,d.XingZuo())
+	//t.Log(nd.Solar2Lunar())
 }
---
+++ date_test.go
@@ -0,0 +1,67 @@
+// 年数在1000-3000
+
+package gocalendar
+
+import (
+	"testing"
+)
+
+var c = NewCtime()
+var d = CtimeNewDate(c)
+
+// 24节气(从立春开始)
+func TestDate_Jieqi(t *testing.T) {
+
+	t.Log(NewCtime().Year(),"年从立春开始的节气时间：")
+	jq := d.Jieqi()
+	for k,v := range(jq){
+		t.Logf("%s:[%s] %d-%d-%d %d:%d:%d\n",k,v.Name,v.Date.Year,v.Date.Month,v.Date.Day,v.Date.Hour,v.Date.Min,v.Date.Sec)
+	}
+}
+
+// 公历的天干地支
+func TestDate_GanZhi(t *testing.T) {
+	/*d = NewDate(&Date{
+		Year:2020,
+		Month:2,
+		Day:4,
+		Hour:20,
+		Min:35,
+		Sec:42,
+		Loc:NewCtime().Location(),
+	})*/
+	Zwz = true
+	d.GanZhi()
+	t.Logf("公历 %d年%d月%d日%d时 转成以立春开始的干支是: %s%s年 %s%s月 %s%s日 %s%s时\n",d.Year,d.Month,d.Day,d.Hour,
+		TianGanArray[d.TianGanDiZhi.Ytg], DiZhiArray[d.TianGanDiZhi.Ydz],
+		TianGanArray[d.TianGanDiZhi.Mtg],DiZhiArray[d.TianGanDiZhi.Mdz],
+		TianGanArray[d.TianGanDiZhi.Dtg],DiZhiArray[d.TianGanDiZhi.Ddz],
+		TianGanArray[d.TianGanDiZhi.Htg],DiZhiArray[d.TianGanDiZhi.Hdz])
+	// t.Logf("%#v\n",dc.TianGanDiZhi)
+}
+
+// 公历转农历，农历转公历
+func TestDate_Solar2Lunar(t *testing.T) {
+	d := NewDate(&Date{
+		Year:2020,
+		Month:1,
+		Day:25,
+		Hour:5,
+		Min:35,
+		Sec:42,
+		Loc:NewCtime().Location(),
+	})
+	l := d.Solar2Lunar()
+	leapMonthStr := ""
+	if l.LeapMonth == 1 {
+		leapMonthStr = "(闰)"
+	}
+	t.Logf("公历%d年%d月%d日转成农历是:%d(%s%s)[%s]年%s%s月%s日",d.Year,d.Month,d.Day,l.Year,l.YearGanZi.Gan,l.YearGanZi.Zhi,l.YearGanZi.Animals, leapMonthStr,MonthChinese(l.Month),DayChinese(l.Day))
+
+	sd := l.Lunar2Solar()
+	t.Logf("农历:%d年%s%s月%s日转成公历是:%d年%d月%d日",l.Year,leapMonthStr,MonthChinese(l.Month),DayChinese(l.Day),sd.Year,sd.Month,sd.Day)
+}
+// 星座
+func TestDate_XingZuo(t *testing.T) {
+	t.Logf("公历%d年%d月%d日是%s座",d.Year,d.Month,d.Day,d.XingZuo())
+}
